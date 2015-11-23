a61665b0a2f0719f0086ff38157c2b3f02a36240
Christian König <christian@koenig.systems>
2015-11-23 13:59:49 +0100

    Wrapped Alamofire calls in a separate function

--- Source/Figo.swift
+++ Source/Figo.swift
@@ -16,40 +16,32 @@ public func login(accessToken accessToken: String) {
 }
 
 public func login(username username: String, password: String, clientID: String, clientSecret: String, completionHandler: (result: Result<Authorization, NSError>) -> Void) {
-    Alamofire.request(Router.LoginUser(username: username, password: password, base64EncodedSecret: base64Encode(clientID: clientID, clientSecret: clientSecret)))
-        .validate()
-        .responseObject() { (result: Result<Authorization, NSError>) -> Void in
-            Session.sharedSession.authorization = result.value
-            completionHandler(result: result)
+    fireRequest(Router.LoginUser(username: username, password: password, secret: base64Encode(clientID, clientSecret))).responseObject() { (result: Result<Authorization, NSError>) -> Void in
+        Session.sharedSession.authorization = result.value
+        completionHandler(result: result)
     }
 }
 
 public func logout(completionHandler: (result: Result<Authorization, NSError>) -> Void) {
-    Alamofire.request(Router.RevokeToken(token: Session.sharedSession.accessToken ?? ""))
-        .validate()
-        .responseObject() { (result: Result<Authorization, NSError>) in
-            Session.sharedSession.authorization = nil
-            completionHandler(result: result)
+    fireRequest(Router.RevokeToken(token: Session.sharedSession.accessToken ?? "")).responseObject() { (result: Result<Authorization, NSError>) in
+        Session.sharedSession.authorization = nil
+        completionHandler(result: result)
     }
 }
 
 public func retrieveAccount(accountID: String, completionHandler: (result: Result<Account, NSError>) -> Void) {
-    Alamofire.request(Router.RetrieveAccount(accountId: accountID))
-        .validate()
-        .responseObject(completionHandler)
+    fireRequest(Router.RetrieveAccount(accountId: accountID)).responseObject(completionHandler)
 }
 
 public func retrieveAccounts(completionHandler: (result: Result<[Account], NSError>) -> Void) {
-    Alamofire.request(Router.RetrieveAccounts)
-        .validate()
-        .responseCollection(completionHandler)
+    fireRequest(Router.RetrieveAccounts).responseCollection(completionHandler)
 }
 
 
 enum Router: URLRequestConvertible {
     private static let baseURLString = "https://api.figo.me"
     
-    case LoginUser(username: String, password: String, base64EncodedSecret: String)
+    case LoginUser(username: String, password: String, secret: String)
     case RefreshToken(token: String)
     case RevokeToken(token: String)
     case RetrieveAccount(accountId: String)
@@ -89,8 +81,8 @@ enum Router: URLRequestConvertible {
         }
         
         switch self {
-            case .LoginUser(let username, let password, let base64EncodedSecret):
-                mutableURLRequest.setValue("Basic \(base64EncodedSecret)", forHTTPHeaderField: "Authorization")
+            case .LoginUser(let username, let password, let secret):
+                mutableURLRequest.setValue("Basic \(secret)", forHTTPHeaderField: "Authorization")
                 return Alamofire.ParameterEncoding.JSON.encode(mutableURLRequest, parameters: ["username": username, "password" : password, "grant_type" : "password"]).0
             case .RefreshToken(let token):
                 return Alamofire.ParameterEncoding.JSON.encode(mutableURLRequest, parameters: ["refresh_token": token]).0
@@ -103,7 +95,11 @@ enum Router: URLRequestConvertible {
 }
 
 
-private func base64Encode(clientID clientID: String, clientSecret: String) -> String {
+private func fireRequest(request: URLRequestConvertible) -> Request {
+    return Alamofire.request(request).validate()
+}
+
+private func base64Encode(clientID: String, _ clientSecret: String) -> String {
     let clientCode: String = clientID + ":" + clientSecret
     let utf8str: NSData = clientCode.dataUsingEncoding(NSUTF8StringEncoding)!
     return utf8str.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.EncodingEndLineWithCarriageReturn)
