4eccf01c164053a91e94d372553aec071ff57eed
Christian König <christian@koenig.systems>
2015-11-24 19:20:08 +0100

    Added more property mappings and types

---
+++ Source/Core/Logging.swift
@@ -0,0 +1,9 @@
+//
+//  Logging.swift
+//  Figo
+//
+//  Created by Christian König on 24.11.15.
+//  Copyright © 2015 CodeStage. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/Resources.swift
@@ -0,0 +1,9 @@
+//
+//  Resources.swift
+//  Figo
+//
+//  Created by Christian König on 24.11.15.
+//  Copyright © 2015 CodeStage. All rights reserved.
+//
+
+import Foundation
