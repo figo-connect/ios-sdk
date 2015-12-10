e20a4a4a1c01d5bd11865c4b340bea632c54a024
Christian König <christian@koenig.systems>
2015-12-10 17:02:27 +0100

    Added a public initializer

--- Source/Request Parameters/RetrieveTransactionsParameters.swift
+++ Source/Request Parameters/RetrieveTransactionsParameters.swift
@@ -33,51 +33,8 @@ public enum TransactionSinceType: String, UnboxableEnum {
 /**
  
 Parameters for RETRIEVE TRANSACTIONS endpoints
- 
-- Note: The default number for the limit parameter is 1000
- 
- 
-- Parameter accounts: **optional** If retrieving the transactions for all accounts, filter the transactions to be only from these accounts
- 
-- Parameter since: **optional** This parameter can either be a transaction ID or an ISO date.
- 
-    *transaction ID*
-     
-    the transactions which are newer than the referenced transaction will be returned in the response. The exact meaning of newer depends on the value of since_type
-     
-    *ISO date*
-     
-    the transactions which were booked on or after this date will be returned in the response
-     
-- Parameter sinceType: **optional** This parameter defines how the parameter since will be interpreted.
-     
-    *booked (default)*
-     
-    the value of the parameter since will be compared to the booking date of the transaction.
-     
-    *created*
-     
-    the value of the parameter since will be compared to the creation time of the transaction on the figo Connect server
-     
-    *modified*
-     
-    the value of the parameter since will be compared to the last modification time of the transaction on the figo Connect server. The response parameter transactions will contain not only newly created transactions but also modified transactions. The response parameter deleted will contain a list of removed transactions.
- 
-- Parameter until: **optional** Return only transactions which were booked on or before this date. Please provide as ISO date
- 
-- Parameter filter: **optional** Filter expression to narrow down the returned transactions
- 
-- Parameter count: **optional** Limit the number of returned transactions. In combination with the offset parameter this can be used to paginate the result list. (default: 1000)
- 
-- Parameter offset: **optional** Offset into the implicit list of transactions used as starting point for the returned transactions. In combination with the count parameter this can be used to paginate the result list.
- 
-- Parameter includePending: **optional** This flag indicates whether pending transactions should be included in the response. Pending transactions are always included as a complete set, regardless of the since parameter. Before caching a copy of the pending transactions, all existing pending transactions for the same account must be removed from the local cache. (default: false)
- 
-- Parameter includeStatistics: **optional** This flag indicates whether statistics over the filtered transaction list should returned alongside the actual transactions. These statistics are always computed on the complete filtered transaction list, i.e. without count and offset. (default: false)
- 
-- Parameter type: **optional** A list of transactions types which the transactions are filtered by.
- 
- */
+
+*/
 public struct RetrieveTransactionsParameters: JSONObjectConvertible {
     
     /// **optional** If retrieving the transactions for all accounts, filter the transactions to be only from these accounts
@@ -126,10 +83,10 @@ public struct RetrieveTransactionsParameters: JSONObjectConvertible {
     public var offset: Int?
     
     /// **optional** This flag indicates whether pending transactions should be included in the response. Pending transactions are always included as a complete set, regardless of the since parameter. Before caching a copy of the pending transactions, all existing pending transactions for the same account must be removed from the local cache. (Default Value: false)
-    public var include_pending: Bool?
+    public var includePending: Bool?
     
     /// **optional** This flag indicates whether statistics over the filtered transaction list should returned alongside the actual transactions. These statistics are always computed on the complete filtered transaction list, i.e. without count and offset. (Default Value: false)
-    public var include_statistics: Bool?
+    public var includeStatistics: Bool?
     
     /// **optional** If true, the balance will be shown in cents
     internal let cents: Bool = true
@@ -137,6 +94,65 @@ public struct RetrieveTransactionsParameters: JSONObjectConvertible {
     /// **optional** A list of transactions types which the transactions are filtered by.
     public var type: [String]?
     
+
+    /**
+     Parameters for RETRIEVE TRANSACTIONS endpoints
+     
+     - Note: The default number for the limit parameter is 1000
+     
+     - Parameter accounts: **optional** If retrieving the transactions for all accounts, filter the transactions to be only from these accounts
+     
+     - Parameter since: **optional** This parameter can either be a transaction ID or an ISO date.
+     
+     *transaction ID*
+     
+     the transactions which are newer than the referenced transaction will be returned in the response. The exact meaning of newer depends on the value of since_type
+     
+     *ISO date*
+     
+     the transactions which were booked on or after this date will be returned in the response
+     
+     - Parameter sinceType: **optional** This parameter defines how the parameter since will be interpreted.
+     
+     *booked (default)*
+     
+     the value of the parameter since will be compared to the booking date of the transaction.
+     
+     *created*
+     
+     the value of the parameter since will be compared to the creation time of the transaction on the figo Connect server
+     
+     *modified*
+     
+     the value of the parameter since will be compared to the last modification time of the transaction on the figo Connect server. The response parameter transactions will contain not only newly created transactions but also modified transactions. The response parameter deleted will contain a list of removed transactions.
+     
+     - Parameter until: **optional** Return only transactions which were booked on or before this date. Please provide as ISO date
+     
+     - Parameter filter: **optional** Filter expression to narrow down the returned transactions
+     
+     - Parameter count: **optional** Limit the number of returned transactions. In combination with the offset parameter this can be used to paginate the result list. (default: 1000)
+     
+     - Parameter offset: **optional** Offset into the implicit list of transactions used as starting point for the returned transactions. In combination with the count parameter this can be used to paginate the result list.
+     
+     - Parameter includePending: **optional** This flag indicates whether pending transactions should be included in the response. Pending transactions are always included as a complete set, regardless of the since parameter. Before caching a copy of the pending transactions, all existing pending transactions for the same account must be removed from the local cache. (default: false)
+     
+     - Parameter includeStatistics: **optional** This flag indicates whether statistics over the filtered transaction list should returned alongside the actual transactions. These statistics are always computed on the complete filtered transaction list, i.e. without count and offset. (default: false)
+     
+     - Parameter type: **optional** A list of transactions types which the transactions are filtered by.
+     
+     */
+    public init(accounts: [String]? = nil, since: String? = nil, sinceType: TransactionSinceType? = nil, until: String? = nil, filter: String? = nil, count: Int? = nil, offset: Int? = nil, includePending: Bool? = nil, includeStatistics: Bool? = nil, type: [String]? = nil) {
+        self.accounts = accounts
+        self.since = since
+        self.sinceType = sinceType
+        self.until = until
+        self.filter = filter
+        self.count = count
+        self.offset = offset
+        self.includePending = includePending
+        self.includeStatistics = includeStatistics
+        self.type = type
+    }
     
     var JSONObject: [String: AnyObject] {
         var dict = Dictionary<String, AnyObject>()
@@ -147,8 +163,8 @@ public struct RetrieveTransactionsParameters: JSONObjectConvertible {
         dict["filter"] = filter
         dict["count"] = count
         dict["offset"] = offset
-        dict["include_pending"] = include_pending
-        dict["include_statistics"] = include_statistics
+        dict["include_pending"] = includePending
+        dict["include_statistics"] = includeStatistics
         dict["cents"] = cents
         dict["type"] = type
         return dict
